c45e7f0a592431a2c0f21a27e1fd663873149d34
Pratyush2710 <pratyushhg@gmail.com>
2020-07-12 01:54:03 +0530

    PeopleCatalog dummy with CSS

--- people-catalog/src/index.js
+++ people-catalog/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const People = ({ img, name, job, children }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  return (
+    <article className="person">
+      <img src={url} alt="People" />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </article>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const PeopleList = () => {
+  return (
+    <section className="person-list">
+      <People img="34" name="Pratyush" job="developer">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, ex?
+        </p>
+      </People>
+      <People img="24" name="Shivam" job="designer">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, ex?
+        </p>
+      </People>
+
+      <People img="44" name="Himanshu" job="Manager">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, ex?
+        </p>
+      </People>
+      <People img="54" name="Umesh" job="tester">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, ex?
+        </p>
+      </People>
+    </section>
+  );
+};
+
+ReactDOM.render(<PeopleList></PeopleList>, document.getElementById("root"));
